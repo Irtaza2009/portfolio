8c67739d72b7417900a59de8d06b7de62e4de5d6
Irtaza <admin@IRT-MacBook-Pro.local>
2024-06-28 12:55:05 +0500

    Moved about me section to the top and added a new article section

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.onmousemove = (e) => handleOnMove(e);
 window.ontouchmove = (e) => handleOnMove(e.touches[0]);
 
 const track = document.getElementById("image-track");
+const articleTrack = document.getElementById("article-track");
 
 const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
 
@@ -44,12 +45,64 @@ const handleOnMove = (e) => {
   }
 };
 
+const handleArticleOnDown = (e) =>
+  (articleTrack.dataset.mouseDownAt = e.clientX);
+
+const handleArticleOnUp = () => {
+  articleTrack.dataset.mouseDownAt = "0";
+  articleTrack.dataset.prevPercentage = articleTrack.dataset.percentage;
+};
+
+const handleArticleOnMove = (e) => {
+  if (articleTrack.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(articleTrack.dataset.mouseDownAt) - e.clientX,
+    maxDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentageUnconstrained =
+      parseFloat(articleTrack.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+  articleTrack.dataset.percentage = nextPercentage;
+
+  articleTrack.animate(
+    {
+      transform: `translate(${nextPercentage}%, -50%)`,
+    },
+    { duration: 1200, fill: "forwards" }
+  );
+
+  for (const article of articleTrack.getElementsByClassName("article-image")) {
+    article.animate(
+      {
+        objectPosition: `${100 + nextPercentage}% center`,
+      },
+      { duration: 1200, fill: "forwards" }
+    );
+  }
+};
+
 window.addEventListener("scroll", () => {
+  const aboutMeSection = document.getElementById("about-me");
+  const aboutMePosition = aboutMeSection.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 1.3;
+
+  if (aboutMePosition < screenPosition) {
+    aboutMeSection.classList.add("visible");
+  }
+
   const portfolioSection = document.getElementById("portfolio");
   const portfolioPosition = portfolioSection.getBoundingClientRect().top;
-  const screenPosition = window.innerHeight / 1.3;
 
   if (portfolioPosition < screenPosition) {
     portfolioSection.classList.add("visible");
+  }
+
+  const articlesSection = document.getElementById("articles");
+  const articlesPosition = articlesSection.getBoundingClientRect().top;
+
+  if (articlesPosition < screenPosition) {
+    articlesSection.classList.add("visible");
   }
 });
